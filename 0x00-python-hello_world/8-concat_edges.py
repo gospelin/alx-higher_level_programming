dcd9547afe13502a70b482e23c11a6eec837bd05
gospelin <gospelin.gi@gmail.com>
2022-06-28 02:18:35 +0100

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# str = "Python is an interpreted, interactive, object-oriented programming\
+# language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
