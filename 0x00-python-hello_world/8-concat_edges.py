68f672842af9bc3f51c308cbd3d47bf3133de3d3
gospelin <gospelin.gi@gmail.com>
2022-06-28 02:26:59 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = f"{str[39:66]} {str[106:110]} {str[:6]}"
+ language that combines remarkable power with very clear syntax"
+str = f"{str[39:66]}{str[106:112]}{str[:6]}"
 print(str)
