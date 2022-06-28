455e4033b1c44a669917fd5e28b33136736e1b15
gospelin <gospelin.gi@gmail.com>
2022-06-28 22:59:15 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(100):
     if number != 99:
-        print("{:02d}, ".format(number), end='')
+        print("{:02d}".format(number), end=', ')
     else:
         print("{:02d}".format(number))
