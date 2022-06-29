f1aedc93872e6225bbb1c51a0957a139c0d1c3b6
gospelin <gospelin.gi@gmail.com>
2022-06-29 13:36:52 +0100

    using f-string

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(100):
     if number != 99:
-        print("{:02d}".format(number), end=', ')
+        print(f"{number:02}, ", end='')
     else:
-        print("{:02d}".format(number))
+       print(f"{number:02}")
