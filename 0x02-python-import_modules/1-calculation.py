dcf048b3b32c33d454968390643c1b9936dbfbac
gospelin <gospelin.gi@gmail.com>
2022-06-30 13:55:16 +0100

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+
+if __name__ == '__main__':
+    main()
