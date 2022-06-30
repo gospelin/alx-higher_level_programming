bd6d3b939757e18ed3869d587834e57bf2e05153
gospelin <gospelin.gi@gmail.com>
2022-06-30 15:20:11 +0100

    Fix Error: Requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,19 +2,24 @@
 from sys import argv
 
 
-length = len(argv) - 1
-word = "arguments"
+def main():
+    length = len(argv) - 1
+    word = "arguments"
 
-if length == 0:
-    print(f"{length} {word}.")
-elif length != 1:
-    print(f"{length} {word}:")
-else:
-    word = "argument"
-    print(f"{length} {word}:")
+    if length == 0:
+        print(f"{length} {word}.")
+    elif length != 1:
+        print(f"{length} {word}:")
+    else:
+        word = "argument"
+        print(f"{length} {word}:")
 
-if length >= 1:
-    for count, args in enumerate(argv, 0):
-        if count == 0:
-            continue
-        print(f"{count}: {args}")
+    if length >= 1:
+        for count, args in enumerate(argv, 0):
+            if count == 0:
+                continue
+            print(f"{count}: {args}")
+
+
+if __name__ == '__main__':
+    main()
