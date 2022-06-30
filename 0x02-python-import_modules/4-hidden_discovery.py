5333aff6e31209836261745723c2980b9f847b08
gospelin <gospelin.gi@gmail.com>
2022-06-30 22:07:21 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def main():
+if __name__ == "__main__":
+    import hidden_4
     for name in dir(hidden_4):
-        if name[0] != '__' and name[1] != '__':
+        if name[0] != '_' and name[1] != '_':
             print(name)
-
-
-if __name__ == '__main__':
-    main()
