f8568148492e823bde70bb782943e07553269390
gospelin <gospelin.gi@gmail.com>
2022-06-30 21:39:36 +0100

    I don't understand

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 
 def main():
     for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+        if name[0] != '__' and name[1] != '__':
             print(name)
 
 
