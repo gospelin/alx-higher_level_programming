64775d22af36899bb46e63ac97cf1bc5fbf1fff1
gospelin <gospelin.gi@gmail.com>
2022-06-30 14:00:45 +0100

    Fix error: operation mismatch

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,9 +7,9 @@ def main():
     b = 5
 
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
 
 if __name__ == '__main__':
