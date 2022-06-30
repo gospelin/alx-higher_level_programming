ca9f428b7a655c53b59fcc18764bfe557029dce9
gospelin <gospelin.gi@gmail.com>
2022-06-30 21:36:50 +0100

    I don't understand

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    for name  in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
+
+
+if __name__ == '__main__':
+    main()
