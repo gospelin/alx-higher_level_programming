f6b5833447aef6007f1653a0170d20c06a23409a
gospelin <gospelin.gi@gmail.com>
2022-06-30 13:27:43 +0100

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,14 +2,9 @@
 from add_0 import add
 
 
-def main():
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    sum = add(a, b)
+sum = add(a, b)
 
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
-
-
-if __name__ == '__main__':
-    main()
+print("{:d} + {:d} = {:d}".format(a, b, sum))
