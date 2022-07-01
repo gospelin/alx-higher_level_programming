34723c6e346dfec43e490945cfb6a44960d2ddf0
gospelin <gospelin.gi@gmail.com>
2022-07-01 07:46:02 +0100

    Refactor: pycodestyle requirement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,11 +3,9 @@
 def delete_at(my_list=[], idx=0):
     if (idx < 0 or idx >= len(my_list)):
         return my_list
-
-    #for index, digit in enumerate(my_list):
-    #    if index == idx:
-    #        my_list.remove(my_list[index])
-
     del my_list[idx]
 
+    # for index, digit in enumerate(my_list):
+    #    if index == idx:
+    #        my_list.remove(my_list[index])
     return my_list
