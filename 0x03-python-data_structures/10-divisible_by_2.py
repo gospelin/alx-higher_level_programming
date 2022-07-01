68b7c861155c4d5fcba4c2e01dba3abf2be52ada
gospelin <gospelin.gi@gmail.com>
2022-07-01 07:32:29 +0100

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    
+    for index, digit in enumerate(my_list):
+        if digit % 2 == 0:
+            new_list[index] = True
+        else:
+            new_list[index] = False
+    return new_list
