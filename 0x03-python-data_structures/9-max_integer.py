6782bc6baa6e1470749938e93de01e58063a7584
gospelin <gospelin.gi@gmail.com>
2022-07-01 07:32:16 +0100

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    
+    max = my_list[0]
+    
+    for index, digit in enumerate(my_list):
+        if my_list[index] > max:
+            max = digit
+    return max
