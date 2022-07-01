1699cf805932f5ab4c7d457e1950c0394b10e532
gospelin <gospelin.gi@gmail.com>
2022-07-01 07:34:36 +0100

    pycodestyle requirement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
+
     max = my_list[0]
-    
+
     for index, digit in enumerate(my_list):
         if my_list[index] > max:
             max = digit
