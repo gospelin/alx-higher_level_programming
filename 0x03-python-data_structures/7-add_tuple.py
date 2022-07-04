1d0649e6b23623cf24cff2af4d10724947658a34
gospelin <gospelin.gi@gmail.com>
2022-07-04 21:11:12 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -21,7 +21,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = tuple_b[0]
         b2 = 0
     else:
-        b1 = tuple_b[1]
+        b1 = tuple_b[0]
         b2 = tuple_b[1]
 
     new_tuple = (a1 + b1, a2 + b2)
