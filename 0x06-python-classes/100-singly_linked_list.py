7542865c3744d8f5de059e37e55c5c78f3d8ce67
gospelin <gospelin.gi@gmail.com>
2022-07-20 23:47:47 +0100

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -19,32 +19,49 @@ class Node:
     As instructed, no method or attribute is created
     """
     def __init__(self, data, next_node=None):
+        """ Method that returns the position value
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ Method that returns the position value
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ Method that returns the position value
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ Method that returns the position value
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ Method that returns the position value
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """ A class that defines a square by its size
+
+    This defines an empty square class.
+    As instructed, no method or attribute is created
+    """
     def __str__(self):
+        """ Method that returns the position value
+        """
         rtn = ""
         ptr = self.__head
 
@@ -57,9 +74,13 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """ Method that returns the position value
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """ Method that returns the position value
+        """
         ptr = self.__head
 
         while ptr is not None:
