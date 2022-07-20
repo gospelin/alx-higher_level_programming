3a52bd95c811a59c58d4eb2e08e159fe972745e2
gospelin <gospelin.gi@gmail.com>
2022-07-20 23:28:57 +0100

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+"""
+
+
 class Square:
     """Class Square that defines a square object
+
+    This defines an empty square class.
+    As instructed, no method or attribute is created
     """
     def __init__(self, size):
         """Initialize method that stores the size of the square
