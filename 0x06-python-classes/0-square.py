a1101ac41a00dc2bce6434141a7bf8cefe3078f6
gospelin <gospelin.gi@gmail.com>
2022-07-20 23:22:21 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+"""
+
+
 class Square:
     """Class Square that defines a square.
 
