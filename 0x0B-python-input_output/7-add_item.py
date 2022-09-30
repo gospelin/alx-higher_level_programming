a00b4965759227458b9840cbb9ca65b2db760ff7
gospelin <gospelin.gi@gmail.com>
2022-09-30 12:12:52 +0100

    Fix: Error in reading empty file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+
 """ Module that adds all arguments to a Python list, and then
 save them to a file
 """
-import sys
+
+from sys import argv
 import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+filename = "add_item.json"
 
 my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+if os.path.exists(filename):
+    if os.path.getsize(filename) == 0:
+        my_list = []
+    else:
+        my_list = load_from_json_file(filename)
 
-save_file(my_list, "add_item.json")
+for args in argv[1:]:
+    my_list.append(args)
+
+save_to_json_file(my_list, filename)
