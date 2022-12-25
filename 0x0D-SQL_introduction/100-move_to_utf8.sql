39598de006e1bd2c4b0d5c53d8e3d7efbdd13f71
gospelin <gospelin.gi@gmail.com>
2022-12-25 20:23:00 +0100

    Query to convert database to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Query to convert database to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
