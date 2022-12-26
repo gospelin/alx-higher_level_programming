f4b57dca22f7bc41ac3aa40482b86dd2aeebf2d6
gospelin <gospelin.gi@gmail.com>
2022-12-26 22:45:05 +0100

    Query to create user with only SELECT privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Query to create new user with only select privilege
+CREATE DATATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
