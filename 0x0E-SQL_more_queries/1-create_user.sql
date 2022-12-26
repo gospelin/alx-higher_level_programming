e36afae7ef606e67fad08cab455ead82140efce2
gospelin <gospelin.gi@gmail.com>
2022-12-26 22:40:10 +0100

    Query to create new user successfully

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Query to create new user if not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
