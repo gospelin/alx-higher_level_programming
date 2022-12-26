ab5d0b8fd571273ecb5d992be5d2215be6871fcc
gospelin <gospelin.gi@gmail.com>
2022-12-26 23:47:25 +0100

    Query with multiple constraints

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Query to create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
