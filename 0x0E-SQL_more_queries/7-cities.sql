52f431d4cdc7c37aa3d1b73bb2ba3de32ed0afcb
gospelin <gospelin.gi@gmail.com>
2022-12-27 00:29:46 +0100

    Foreign key constraint

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- Query: Multiple constraints
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	state_id NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
