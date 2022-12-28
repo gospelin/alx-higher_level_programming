fee5c67e30ee25d84ff764b49cde26ac8cee1ef0
gospelin <gospelin.gi@gmail.com>
2022-12-28 07:32:04 +0100

    Query that lists all shows that have zero or more genre linked

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Query that lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
