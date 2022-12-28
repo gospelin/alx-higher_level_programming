50204d7c22523fb0fc9ca3b246cb80bfd89173f9
gospelin <gospelin.gi@gmail.com>
2022-12-28 07:30:30 +0100

    Query that lists all shows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Query that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows 
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
