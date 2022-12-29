1152ea9aa016f5c6a09b2ca6c26a221faf23b018
gospelin <gospelin.gi@gmail.com>
2022-12-29 23:49:49 +0100

    Query that lists all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- Query that lists all genres of the show Dexter.
+SELECT tv_genres.name 
+FROM tv_genres 
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = "Dexter");
