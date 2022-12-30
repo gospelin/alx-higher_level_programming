7f073818c09aadc9d1736cd06a91773b2760e951
gospelin <gospelin.gi@gmail.com>
2022-12-30 01:34:45 +0100

    Query that lists all shows from hbtn_0d_tvshows_rate by their rating.

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- Query that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(rating)
+SELECT tv_shows.title, SUM(rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
