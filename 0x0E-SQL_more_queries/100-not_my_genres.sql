82e3cff1c564815177c0ec83dbf6de7706b45fd6
gospelin <gospelin.gi@gmail.com>
2022-12-30 01:18:25 +0100

    Query that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- Query that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+  SELECT tv_show_genres.genre_id
+  FROM tv_shows
+  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+  WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
