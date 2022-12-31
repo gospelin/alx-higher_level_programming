178dfa086c82fe59d6a0808bf85f666aef16599d
gospelin <gospelin.gi@gmail.com>
2023-01-01 00:19:18 +0100

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let num = process.argv[2];
+num = Number(num);
+
+if ((num === undefined) || isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My Number: ${num}`);
+}
