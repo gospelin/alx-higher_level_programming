6396d5423dc10937c24c23cfdfd8d1915d833eb9
gospelin <gospelin.gi@gmail.com>
2023-01-01 02:37:19 +0100

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
-
-if (isNaN(num)) {
-  console.log(1);
-}
-
 const fact = factorial(num);
 console.log(fact);
 
 function factorial (a) {
-  if (a <= 1) {
+  if (isNaN(a) || a <= 1) {
     return 1;
   }
   return (a * factorial(a - 1));
