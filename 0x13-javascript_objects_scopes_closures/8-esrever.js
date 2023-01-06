4573f39cf5ccd4feac13ee5e082b156697e3a5f1
gospelin <gospelin.gi@gmail.com>
2023-01-06 05:52:29 +0100

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const newList = [];
+
+  for (let i = (list.length - 1); i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
