92d0476404ba4f0d5f6b5f5785bde7fd369a6c64
gospelin <gospelin.gi@gmail.com>
2023-01-06 04:41:34 +0100

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      let row = '';
+      for (let j = 1; j <= this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+}
+
+module.exports = Rectangle;
