8586b961238b8ae68c5c6781d178161ec8770785
gospelin <gospelin.gi@gmail.com>
2023-01-09 02:57:53 +0100

    Python: Query to list all rows from states table in Ascending order by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Query to select all rows from states table.
-
-The Query performs a simple task of printing out as a tuple the data.
+List all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
