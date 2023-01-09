a10278ebdc06357297e305d63ddc5d427166379f
gospelin <gospelin.gi@gmail.com>
2023-01-09 02:46:18 +0100

    Python: Query to list all rows from states table in Ascending order by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Query to select all rows from states table."""
 import MySQLdb
 from sys import argv
 
@@ -7,8 +8,12 @@ db_key = []
 for args in argv[1:]:
     db_key.append(args)
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=f"{db_key[0]}",
-                       passwd=f"{db_key[1]}", db=f"{db_key[2]}")
+conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306, user=f"{db_key[0]}",
+                        passwd=f"{db_key[1]}",
+                        db=f"{db_key[2]}"
+                      )
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
