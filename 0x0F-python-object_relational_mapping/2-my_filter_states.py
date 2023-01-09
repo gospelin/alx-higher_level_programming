26976799d134674c4fbc22c781b3bddd090a4d08
gospelin <gospelin.gi@gmail.com>
2023-01-09 04:55:35 +0100

    List all states that matches argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+List all states that matches argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db_key = []
+
+    for args in argv[1:]:
+        db_key.append(args)
+
+    db = MySQLdb.connect(
+      host="localhost",
+      port=3306,
+      user=f"{db_key[0]}",
+      passwd=f"{db_key[1]}",
+      db=f"{db_key[2]}"
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+      WHERE name LIKE \
+      '%{}%' ORDER BY states.id ASC".format(db_key[3]))
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
