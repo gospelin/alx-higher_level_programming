716d2cd06ce5243a64ca915b30fdc75a001d4e7f
gospelin <gospelin.gi@gmail.com>
2023-01-09 02:39:04 +0100

    Python: Query to list all states in db ASC by id

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+db_key = []
+
+for args in argv[1:]:
+    db_key.append(args)
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=f"{db_key[0]}",
+                       passwd=f"{db_key[1]}", db=f"{db_key[2]}")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+query_rows = cur.fetchall()
+
+for row in query_rows:
+    print(row)
+
+cur.close()
+conn.close()
