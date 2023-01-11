5f7c04f3627457aae887194babfc587a52d62776
gospelin <gospelin.gi@gmail.com>
2023-01-12 00:16:52 +0100

    List all cities in states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+List all cities in states from database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    # Connect to Database
+    db = MySQLdb.connect(
+      host="localhost",
+      port=3306,
+      user=user,
+      passwd=password,
+      db=db
+    )
+
+    # Create a pointer to database
+    cur = db.cursor()
+    sql = """SELECT cities.id, cities.name, states.name FROM cities
+          INNER JOIN states WHERE states.id = state_id
+          ORDER BY cities.id ASC LIMIT 15"""
+
+    # Execute sql command
+    cur.execute(sql)
+
+    # Store the output from database
+    states = cur.fetchall()
+
+    # Display the result
+    for state in states:
+        print(state)
+
+    # Close database connection
+    cur.close()
+    db.close()
