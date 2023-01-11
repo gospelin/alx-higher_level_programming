0eb7e87157416fc2c062da50216c0293d0ca716a
gospelin <gospelin.gi@gmail.com>
2023-01-12 00:45:00 +0100

    List all cities in states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     # Create a pointer to database
     cur = db.cursor()
     sql = """SELECT cities.id, cities.name, states.name FROM cities
-          CROSS JOIN states WHERE states.id = state_id
-          ORDER BY cities.id AS"""
+          INNER JOIN states ON cities.state_id = states.id
+          ORDER BY cities.id ASC"""
 
     # Execute sql command
     cur.execute(sql)
